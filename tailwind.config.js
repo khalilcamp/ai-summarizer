a0c429dbedf0511c069ddd57b8734cb293a23942
Khalil Camargo <kacamargopacker@gmail.com>
2024-06-19 22:14:15 -0300

    50% done

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"]
+      }
+    },
+  },
+  plugins: [],
+}
